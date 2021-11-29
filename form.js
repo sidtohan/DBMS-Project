eafa42dee8c455e995a176d5f8a0a17df603d2cf
sidtohan <siddhant.tohan@gmail.com>
2021-11-29 21:13:29 +0530

    Completed the login form now, looks good

--- form.js
+++ form.js
@@ -2,29 +2,53 @@ const formHandler = () => {
   const loginOptions = document.querySelector(".login-options");
   const generateForm = (type) => {
     const loginForm = document.createElement("form");
-    const id = document.createElement("input");
-    id.type = "text";
-    id.placeholder = "ID";
-    id.required = "";
 
+    const idDiv = document.createElement("div");
+    const idHeading = document.createElement("h3");
+    const id = document.createElement("input");
+    idHeading.textContent = "ID";
+    idHeading.classList.add("id-heading");
+    id.type = "text";
+    id.required = "";
+    id.name = "id";
+    idDiv.appendChild(idHeading);
+    idDiv.appendChild(id);
+
+    const nameDiv = document.createElement("div");
+    const nameHeading = document.createElement("h3");
     const name = document.createElement("input");
+
+    if (type == "patient" || type == "doctor") {
+      nameHeading.textContent = "First Name";
+    } else if (type == "hospital") {
+      nameHeading.textContent = "Hospital Name";
+    } else {
+      nameHeading.textContent = "Supplier Name";
+    }
+
+    nameHeading.classList.add("name-heading");
     name.type = "text";
-    name.placeholder = "Name";
     name.required = "";
+    name.name = "name";
+    nameDiv.appendChild(nameHeading);
+    nameDiv.appendChild(name);
 
     const formType = document.createElement("input");
     formType.type = "text";
     formType.defaultValue = type;
     formType.classList.add("hide-pls");
-
+    formType.name = "type";
     const submitButton = document.createElement("button");
     submitButton.type = "submit";
-    submitButton.textContent = "Submit";
+
+    const arrowImage = new Image();
+    arrowImage.src = "./assets/right-arrow.svg";
+    submitButton.appendChild(arrowImage);
 
     loginForm.classList.add("login-form");
     loginForm.appendChild(formType);
-    loginForm.appendChild(id);
-    loginForm.appendChild(name);
+    loginForm.appendChild(idDiv);
+    loginForm.appendChild(nameDiv);
     loginForm.appendChild(submitButton);
     return loginForm;
   };
@@ -36,12 +60,7 @@ const formHandler = () => {
   const addForm = (type) => {
     clearArea();
     const form = generateForm(type);
-    const heading = document.createElement("div");
-    heading.textContent = "Details";
-    heading.classList.add("form-heading");
-
     loginOptions.classList.add("login-active");
-    loginOptions.appendChild(heading);
     loginOptions.appendChild(form);
   };
 
